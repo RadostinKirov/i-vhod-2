25fc652e60e23248ae3a4f6ec8b7bbea1348ce16
Radostin Kirov <rado@elevatecompany.eu>
2023-04-23 14:50:18 +0300

    layout created

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,32 +9,9 @@ export default function Home() {
   return (
 
     <>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <link
-          href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900"
-          rel="stylesheet"
-        />
-        <title>Grad School HTML5 Template</title>
-        {/* Bootstrap core CSS */}
-        <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-        {/* Additional CSS Files */}
-        <link rel="stylesheet" href="/css/fontawesome.css" />
-        <link rel="stylesheet" href="/css/templatemo-grad-school.css" />
-        <link rel="stylesheet" href="/css/owl.css" />
-        <link rel="stylesheet" href="/css/lightbox.css" />
-      </Head>
+        <div className="main-container">
 
-      <div className="main-container">
-
-        <Header />
-        {/* ***** Main Banner Area Start ***** */}
+              {/* ***** Main Banner Area Start ***** */}
 
 
         <section className="section main-banner" id="top" data-section="section1">
@@ -674,12 +651,7 @@ export default function Home() {
           </div>
         </section>
 
-        <Footer />
-
-        {/* Scripts */}
-        {/* Bootstrap core JavaScript */}
-
-      </div>
+            </div>
     </>
 
   )
